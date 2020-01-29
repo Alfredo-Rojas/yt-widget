d7288a8dffa4e546b820ecd7298c55f442394808
Alfredo <alfredot84@gmail.com>
2020-01-29 10:35:59 -0600

    Add show button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [show, setShow] = useState(true);
+
   return (
-    <div class="card-panel">
-      <span class="blue-text text-darken-2">
-        This is a card panel with dark blue text
-      </span>
+    <div className="container">
+      <div className="row">
+        <div className="col s12 m8 offset-m2 l4 offset-l4 widget-container">
+          {show && (
+            <div className="widget">
+              <span className="blue-text text-darken-2">
+                This is a card panel with dark blue text
+              </span>
+            </div>
+          )}
+          {show && <div className="hr" />}
+          <div className="widget-hider" onClick={() => setShow(!show)}>
+            {show ? "Hide Chat" : "Show Chat"}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
