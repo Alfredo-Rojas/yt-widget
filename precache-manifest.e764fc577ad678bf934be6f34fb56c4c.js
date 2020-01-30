0d5f821448bc6ff54553eeb4b7cfbc56af2acbc6
Alfredo-Rojas <alfredot84@gmail.com>
2020-01-30 10:17:39 +0800

    Updates

---
+++ precache-manifest.e764fc577ad678bf934be6f34fb56c4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d94c7252df868771d41e10bc8f64de",
+    "url": "/yt-widget/index.html"
+  },
+  {
+    "revision": "14f0c72e1ea92547342b",
+    "url": "/yt-widget/static/css/main.ba9cc143.chunk.css"
+  },
+  {
+    "revision": "4b98505198f06b9c2365",
+    "url": "/yt-widget/static/js/2.9b575496.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/yt-widget/static/js/2.9b575496.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14f0c72e1ea92547342b",
+    "url": "/yt-widget/static/js/main.137ef6c4.chunk.js"
+  },
+  {
+    "revision": "e1276a3da87b01e3ebbe",
+    "url": "/yt-widget/static/js/runtime-main.ca839aa4.js"
+  }
+]);
